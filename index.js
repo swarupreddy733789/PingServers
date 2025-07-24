5ab505350f9bd33895e79e5f173c211490bcbba1
swarupreddy733789 <swarupreddynallamilli@gmail.com>
2025-07-24 15:49:19 +0530

    fenfe

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const cron = require('node-cron');
+const axios = require('axios');
+
+const server = express();
+
+server.get("/status", (req, res) => {
+    return res.status(200).json({
+        message: "hello from server"
+    });
+});
+
+cron.schedule('*/50 * * * * *', async () => {
+    try {
+        const response = axios.get('https://eureka-server-latest-8g6s.onrender.com');
+        console.log("first");
+    }
+    catch(error) {
+        console.log('ping failed: ', error.message);
+    }
+});
+
+
+
+server.listen(1000, () => {
+    console.log("server stated.");
+})
